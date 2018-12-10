39238ed6b232a4679ff3ee5bdb34b6bb1da13d04
GeovaniPrestes <42949288+GeovaniPrestes@users.noreply.github.com>
2018-12-10 17:06:47 -0200

    Blog Antigo

---
+++ js.js
@@ -0,0 +1,27 @@
+var slideIndex = 0;
+carousel();
+
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 1000); // Change image every 2 seconds
+}
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides2");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1} 
+    x[slideIndex-1].style.display = "block"; 
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
